058cfd53992ab96706352bdbd4776c09d765319f
Stephanie <stephnjonjo@gmail.com>
2024-10-06 17:33:30 +0300

    adds new lines of code

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
+const cats = ["Milo", "Otis", "Garfield"]
+
+// destructive functions
+function destructivelyAppendCat(Milo){
+    cats.push(Milo)
+}
+
+function destructivelyPrependCat(Garfield){
+    cats.unshift(Garfield)
+}
+
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield)
+}
+
+function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+}
+
+//non-destructive functions
+function appendCat(name){
+    return [...cats,name]
+}
+
+function prependCat(name){
+    return [name, ...cats]
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length - 1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
+
+
+
+
 // Write your solution here!
